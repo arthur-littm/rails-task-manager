b19150f152b65dd4d772e96e8a679dab254722be
Arthur Littmann <arthur.littmann@gmail.com>
2017-05-22 19:59:41 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   # get 'tasks/destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tasks
+  # resources :tasks
 end
